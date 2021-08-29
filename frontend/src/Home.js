261504a13ddb499772daf3aab7b8adae2b9765a1
Kristen Krofchik <k.krofchik@gmail.com>
2021-08-29 00:33:42 -0500

    changes to home

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import UserContext from './UserContext';
 
-function Home({currentUser}) {
+function Home() {
+    const { currentUser } = useContext(UserContext);
+
     return (
         <div className='Home'>
             <h1>Jobly</h1>
             <p>All the jobs in one convenient place.</p>
-            <h2>Welcome Back, {currentUser}</h2>
+            {currentUser 
+                ? <h2>Welcome Back, {currentUser.username}</h2>
+                : (
+                    <div>
+                        <Link to='/login'>Log In</Link>
+                        <Link to='/signup'>Sign Up</Link>
+                    </div>
+                )}
         </div>
     )
 }
