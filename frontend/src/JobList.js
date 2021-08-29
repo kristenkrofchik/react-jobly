2e19f9c9a0d972c1d6bca21c6f99ce8d8bdb74a2
Kristen Krofchik <k.krofchik@gmail.com>
2021-08-28 23:35:59 -0500

    jobs work

---
+++ frontend/src/JobList.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import JoblyApi from '../api';
+import JobCard from './JobCard';
+
+import SearchForm from './SearchForm';
+
+function JobList() {
+    const [jobs, setJobs] = useState(null);
+
+    useEffect(function getJobsList() {
+        searchBarName();
+    }, []);
+
+    async function searchBarName(title) {
+        let jobs = await JoblyApi.getJobs(title);
+        setJobs(jobs);
+    } 
+
+    return (
+        <div>
+            <SearchForm searchFor={searchBarName} />
+            <div>
+                {jobs.map(j => (
+                    <JobCard key={j.id} title={j.title} salary={j.salary} equity={j.equity} companyHandle={j.companyHandle} />
+                ))}
+            </div>
+        </div>
+    )
+
+}
+
+export default JobList;
