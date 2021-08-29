02cb50617dfdd1cf53053c9cc8913ad36cc330f5
Kristen Krofchik <k.krofchik@gmail.com>
2021-08-29 00:07:33 -0500

    jobcard work

--- frontend/src/JobCard.js
+++ frontend/src/JobCard.js
@@ -1,1 +1,33 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import UserContext from '/UserContext';
+
+function JobCard({id, title, salary, equity, companyHandle}) {
+
+    const [hasAppliedToJob, applyToJob] = useContext(UserContext);
+    const [applied, setApplied] = useState();
+
+    useEffect(function updateApplied() {
+        setApplied(hasAppliedToJob(id));
+    }, [id, hasAppliedToJob]);
+
+    async function handleApply(evt) {
+        evt.preventDefault();
+        if(hasAppliedToJob(id)) return;
+        applyToJob(id);
+        setApplied(true);
+    }
+ 
+    return (
+        <div className='JobCard'>
+            <h2>{title}</h2>
+            <h4>{companyHandle}</h4>
+            <h5>Salary: {salary}</h5>
+            <h5>Equity: {equity}</h5>
+            <button onClick={handleApply} disabled={applied}>
+                {applied ? "Applied" : "Apply"}
+            </button>
+        </div>
+    )
+}
+
+export default JobCard;
